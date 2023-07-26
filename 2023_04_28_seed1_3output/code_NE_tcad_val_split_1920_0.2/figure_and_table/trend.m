817393fedcb22e795913de74f8a6323f00779567
BING-JU <61406893+BING-JU@users.noreply.github.com>
2023-07-26 18:42:49 +0800

    Add files via upload

---
+++ 2023_04_28_seed1_3output/code_NE_tcad_val_split_1920_0.2/figure_and_table/trend.m
@@ -0,0 +1,251 @@
+clc;
+clear;
+trend_line = readtable('trend_doseenergy1.csv');
+trend_line = table2array(trend_line);
+sample_rate = 10;
+figure_num = length(trend_line) / sample_rate;
+for i = 1:figure_num
+    % linear
+    figure('units','centimeter','position',[8, 4, 12, 10])
+    x = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 5);
+    y_true_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13);
+    y_true_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 14);
+    y_true_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 15);
+    y_NE_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 7);
+    y_NE_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 8);
+    y_NE_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 9);
+    y_mlp_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 10);
+    y_mlp_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 11);
+    y_mlp_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 12);
+    y_min = min(min(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    y_max = max(max(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    simulate = plot(x, y_true_2, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_3, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_4, 'bo', 'MarkerSize', 5);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_2, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_3, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_4, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_2, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_3, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_4, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    
+    set(gca,'FontSize', 20, 'LineWidth', 4.0,'fontweight','bold')
+    xlim([20, 420]),ylim([(y_min/2) (y_max + y_min*2)]), ytickformat('%1.2f')%,xticks(0:0.3:0.9), yticks(0:5e-4:1e-3);
+    xlabel('\bfEn_d_o_s_e_2(eV)', 'FontSize',20), ylabel('\bfI_c (A) ', 'FontSize',20)
+    legend([pred_mlp, pred_NE, simulate], {'\bfMLP', '\bfNE', '\bfTrue'},'FontSize',16, 'Location','southeast','Box','off');
+    
+%     % log scale
+    figure('units','centimeter','position',[8, 4, 12, 10])
+    x = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 5);
+    y_true_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13);
+    y_true_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 14);
+    y_true_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 15);
+    y_NE_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 7);
+    y_NE_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 8);
+    y_NE_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 9);
+    y_mlp_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 10);
+    y_mlp_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 11);
+    y_mlp_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 12);
+    y_min = min(min(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    y_max = max(max(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    simulate = plot(x, y_true_2, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_3, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_4, 'bo', 'MarkerSize', 5);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_2, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_3, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_4, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_2, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_3, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_4, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    
+    set(gca,'FontSize', 20, 'LineWidth', 4.0,'YScale','log','fontweight','bold');
+    xlim([20, 420]),ylim([(y_min/2) (y_max + y_min*10)]), ytickformat('%1.1f'), yticks(1e-4:9e-4:1e-3);%,xticks(0:0.3:0.9), yticks(0:5e-4:1e-3);
+    xlabel('\bfEn_d_o_s_e_2(eV)', 'FontSize',20), ylabel('\bfI_c (A) ', 'FontSize',20)
+    legend([pred_mlp, pred_NE, simulate], {'\bfMLP', '\bfNE', '\bfTrue'},'FontSize',16, 'Location','southeast','Box','off');
+end
+
+
+trend_line = readtable('trend_doseenergy2.csv');
+trend_line = table2array(trend_line);
+sample_rate = 8;
+figure_num = length(trend_line) / sample_rate;
+for i = 1:figure_num
+    % linear
+    figure('units','centimeter','position',[8, 4, 12, 10])
+    x = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 6);
+    y_true_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13);
+    y_true_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 14);
+    y_true_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 15);
+    y_NE_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 7);
+    y_NE_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 8);
+    y_NE_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 9);
+    y_mlp_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 10);
+    y_mlp_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 11);
+    y_mlp_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 12);
+    y_min = min(min(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    y_max = max(max(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    simulate = plot(x, y_true_2, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_3, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_4, 'bo', 'MarkerSize', 5);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_2, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_3, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_4, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_2, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_3, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_4, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    
+    set(gca,'FontSize', 20, 'LineWidth', 4.0,'fontweight','bold')
+    xlim([20, 110]),ylim([(y_min/1.2) (y_max + y_min)]), ytickformat('%1.1f')%,xticks(0:0.3:0.9), yticks(0:5e-4:1e-3);
+    xlabel('\bfEn_d_o_s_e_1(eV)', 'FontSize',20), ylabel('\bfI_c (A) ', 'FontSize',20)
+    legend([pred_mlp, pred_NE, simulate], {'\bfMLP', '\bfNE', '\bfTrue'},'FontSize',16, 'Location','northwest','Box','off');
+    
+%     % log scale
+    figure('units','centimeter','position',[8, 4, 12, 10])
+    x = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 6);
+    y_true_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13);
+    y_true_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 14);
+    y_true_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 15);
+    y_NE_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 7);
+    y_NE_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 8);
+    y_NE_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 9);
+    y_mlp_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 10);
+    y_mlp_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 11);
+    y_mlp_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 12);
+    y_min = min(min(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    y_max = max(max(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    simulate = plot(x, y_true_2, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_3, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_4, 'bo', 'MarkerSize', 5);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_2, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_3, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_4, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_2, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_3, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_4, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    
+    set(gca,'FontSize', 20, 'LineWidth', 4.0,'YScale','log','fontweight','bold');
+    xlim([20, 110]),ylim([(y_min/1.2) (y_max + y_min)]), ytickformat('%1.2f')%,xticks(0:0.3:0.9), yticks(0:5e-4:1e-3);
+    xlabel('\bfEn_d_o_s_e_1(eV)', 'FontSize',20), ylabel('\bfI_c (A) ', 'FontSize',20)
+    legend([pred_mlp, pred_NE, simulate], {'\bfMLP', '\bfNE', '\bfTrue'},'FontSize',16, 'Location','northwest','Box','off');
+end
+
+trend_line = readtable('trend_cdose2.csv');
+trend_line = table2array(trend_line);
+sample_rate = 6;
+figure_num = length(trend_line) / sample_rate;
+for i = 1:figure_num
+    % linear
+    figure('units','centimeter','position',[8, 4, 12, 10])
+    x = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 3);
+    y_true_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13);
+    y_true_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 14);
+    y_true_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 15);
+    y_NE_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 7);
+    y_NE_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 8);
+    y_NE_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 9);
+    y_mlp_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 10);
+    y_mlp_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 11);
+    y_mlp_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 12);
+    y_min = min(min(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    y_max = max(max(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    simulate = plot(x, y_true_2, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_3, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_4, 'bo', 'MarkerSize', 5);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_2, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_3, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_4, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_2, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_3, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_4, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    
+    set(gca,'FontSize', 20, 'LineWidth', 4.0,'fontweight','bold')
+    xlim([4.5E15, 11E15]),ylim([(y_min/1.2) (y_max + y_min)]), ytickformat('%1.1f')%,xticks(0:0.3:0.9), yticks(0:5e-4:1e-3);
+    xlabel('\bfC_d_o_s_e_2(cm^-^2)', 'FontSize',20), ylabel('\bfI_c (A) ', 'FontSize',20)
+    legend([pred_mlp, pred_NE, simulate], {'\bfMLP', '\bfNE', '\bfTrue'},'FontSize',16, 'Location','northwest','Box','off');
+    
+%     % log scale
+    figure('units','centimeter','position',[8, 4, 12, 10])
+    x = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 3);
+    y_true_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13);
+    y_true_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 14);
+    y_true_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 15);
+    y_NE_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 7);
+    y_NE_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 8);
+    y_NE_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 9);
+    y_mlp_pred_2 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 10);
+    y_mlp_pred_3 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 11);
+    y_mlp_pred_4 = trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 12);
+    y_min = min(min(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    y_max = max(max(trend_line(1 + (i - 1) * sample_rate:  i * sample_rate, 13:15)));
+    simulate = plot(x, y_true_2, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_3, 'bo', 'MarkerSize', 5);
+    hold on
+    simulate = plot(x, y_true_4, 'bo', 'MarkerSize', 5);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_2, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_3, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_mlp = plot(x, y_mlp_pred_4, 'Color', 'r', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_2, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_3, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    pred_NE = plot(x, y_NE_pred_4, 'Color', 'b', 'LineWidth', 1);
+    hold on
+    
+    set(gca,'FontSize', 20, 'LineWidth', 4.0,'YScale','log','fontweight','bold');
+    xlim([4.5E15, 11E15]),ylim([(y_min/1.2) (y_max + y_min)]), ytickformat('%1.1f')%,xticks(0:0.3:0.9), yticks(0:5e-4:1e-3);
+    xlabel('\bfC_d_o_s_e_2(cm^-^2)', 'FontSize',20), ylabel('\bfI_c (A) ', 'FontSize',20)
+    legend([pred_mlp, pred_NE, simulate], {'\bfMLP', '\bfNE', '\bfTrue'},'FontSize',16, 'Location','northwest','Box','off');
+end
+
+
+
---
+++ 2023_04_28_seed1_3output/code_NE_tcad_val_split_1920_0.2/generate_dataset/generate_dataset.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import shutil
+
+result = pd.read_csv("result_v3.csv", index_col=False)
+result_144 = pd.read_csv("result_0501.csv", index_col=False)
+seed = 1
+
+# v4_version and v5
+# delete
+dopant_value = [1, 2]
+cdose1_values = [1E15, 5E15]
+doseenergy1_values = [40, 80, 120, 160, 200, 240, 280, 320, 360, 400]
+doseenergy2_values = [30, 40, 50, 60, 70, 80, 90, 100]
+dopant_filter = result["dopant"].isin(dopant_value)
+cdose1_filter = result["Cdose1(cm^-2)"].isin(cdose1_values)
+doseenergy1_filter = result["DoseEnergy1(KeV)"].isin(doseenergy1_values)
+doseenergy2_filter2 = result["DoseEnergy2(KeV)"].isin(doseenergy2_values)
+dataset = result[dopant_filter & cdose1_filter & doseenergy1_filter & doseenergy2_filter2]
+dataset_drop = dataset.head(288)# 1920 2880
+dataset = dataset.drop(index=dataset_drop.index)
+# # register v5
+# dopant_value = [1]
+# cdose1_values = [1E15, 5E15]
+# cdose2_values = [5E20]
+# doseenergy1_values = [320, 360, 400]
+# doseenergy2_values = [30,40,50,60,70, 80, 90, 100]
+# dopant_filter = result["dopant"].isin(dopant_value)
+# cdose1_filter = result["Cdose1(cm^-2)"].isin(cdose1_values)
+# cdose2_filter = result["Edose(cm^-2)"].isin(cdose2_values)
+# doseenergy1_filter = result["DoseEnergy1(KeV)"].isin(doseenergy1_values)
+# doseenergy2_filter2 = result["DoseEnergy2(KeV)"].isin(doseenergy2_values)
+# register = result[dopant_filter & cdose1_filter & cdose2_filter & doseenergy1_filter & doseenergy2_filter2]
+# dataset = pd.concat([dataset, register, result_144])
+
+
+dataset = dataset.sample(frac=1, random_state=seed)
+
+
+# random
+test_set = dataset.sample(frac=0.8, random_state=seed)
+train_set = dataset.drop(index=test_set.index)
+
+test_set.to_csv("test_set.csv", index=False)
+train_set.to_csv("train_set.csv", index=False)
+dataset = dataset.sort_values(by=['dopant','Cdose1(cm^-2)','Cdose2(cm^-2)', 'Edose(cm^-2)', 'DoseEnergy1(KeV)', 'DoseEnergy2(KeV)'])
+dataset.to_csv("dataset.csv", index=False)
+
+shutil.copy("test_set.csv", "../train_model/dataset/test_set.csv")
+shutil.copy("train_set.csv", "../train_model/dataset/train_set.csv")
+shutil.copy("dataset.csv", "../train_model/dataset/dataset.csv")
+
+
