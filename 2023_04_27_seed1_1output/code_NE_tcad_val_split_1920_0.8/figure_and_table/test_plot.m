33550a7fbc66da63bc4c5165502419cf7d360b2e
BING-JU <61406893+BING-JU@users.noreply.github.com>
2023-07-26 17:40:11 +0800

    Add files via upload

---
+++ 2023_04_27_seed1_1output/code_NE_tcad_val_split_1920_0.8/figure_and_table/test_plot.m
@@ -0,0 +1,44 @@
+clc;
+close all;
+clear all;
+generation = 10;
+indiv = 10;
+data = readtable("./NE_performance.csv");
+data = table2array(data);
+data_mlp = readtable("./mlp_performance.csv");
+data_mlp = table2array(data_mlp);
+
+figure('units','centimeter','position',[8, 4, 12, 10])
+data_y_mlp = data_mlp(2:end, 1) * 100;
+for i=1:generation
+    data_y = 100 * data(2:end, i);
+    data_x = i * ones(indiv,1);
+    scatter(data_x, data_y, 60,'filled');
+    hold on
+end
+mlp = yline(data_y_mlp,'-','MLP', 'Color', 'black', 'LineWidth', 1.5);
+set(gca,'FontSize', 20, 'LineWidth', 4.0)
+
+xlim([0 generation + 1]);
+% ylim([5.43 5.7]);
+xlabel('\bfGeneration', 'FontSize',20);
+ylabel('\bfTest set(mape)%', 'FontSize',20);
+legend('Individual in each generation','FontSize',14, 'Location','northeast','Box','off')
+box on;
+
+figure('units','centimeter','position',[8, 4, 12, 10])
+data_x = linspace(1, generation, generation);
+data_y = data(2,1:end) * 100;
+data_y_mlp = data_mlp(2:end, 1) * 100;
+NE = plot(data_x, data_y,'-b.', 'MarkerSize',20, 'LineWidth', 1.5);
+hold on 
+mlp = yline(data_y_mlp,'-','MLP', 'Color', 'black', 'LineWidth', 1.5);%plot(data_x, data_y_mlp, 'Color', 'black', 'LineWidth', 1.5);
+set(gca,'FontSize', 20, 'LineWidth', 4.0)
+
+xlim([0 generation + 1]);
+ylim([0 2*data_y_mlp]);
+xlabel('\bfGeneration', 'FontSize',20);
+ylabel('\bfTest set(mape)%', 'FontSize',20);
+legend('dataset trend','FontSize',14, 'Location','northeast','Box','off')
+legend([NE, mlp], {'\bfNeuroEvolution','\bfMLP'},'FontSize',14, 'Location','northeast','Box','off');
+box on;
